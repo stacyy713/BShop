e37a547b5a6d5e205d2a1a512fd6bce7570136ae
Albina3107 <71102725+Albina3107@users.noreply.github.com>
2021-12-28 00:52:15 +0200

    Add files via upload

--- src/models/models.js
+++ src/models/models.js
@@ -1,30 +1,40 @@
-const sequelize = require('../db');
-const {DataTypes} = require('sequelize');
+const sequelize = require("../db");
+const { DataTypes } = require("sequelize");
 
-const User = sequelize.define('user', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    usr_login: {type: DataTypes.STRING, unique: true},
-    password: {type: DataTypes.STRING},
-    role: {type: DataTypes.STRING, defaultValue: "USER"}
+const User = sequelize.define("user", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  usr_login: { type: DataTypes.STRING, unique: true },
+  password: { type: DataTypes.STRING },
+  role: { type: DataTypes.STRING, defaultValue: "USER" },
 });
 
-const Basket = sequelize.define('basket', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
+const Basket = sequelize.define("basket", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 });
 
-const BasketBook = sequelize.define('basket_book', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
+const BasketBook = sequelize.define("basket_book", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 });
 
-const Book = sequelize.define('book', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name: {type: DataTypes.STRING, unique: true, allowNull: false},
-    author: {type: DataTypes.STRING, allowNull: false},
-    year: {type: DataTypes.INTEGER},
-    genre: {type: DataTypes.STRING},
-    originality: {type: DataTypes.STRING},
-    price: {type: DataTypes.INTEGER, allowNull: false},
-    img: {type: DataTypes.STRING, allowNull: false}
+const Book = sequelize.define("book", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+  author: { type: DataTypes.STRING, allowNull: false },
+  year: { type: DataTypes.INTEGER },
+  genre: { type: DataTypes.STRING },
+  originality: { type: DataTypes.STRING },
+  price: { type: DataTypes.INTEGER, allowNull: false },
+  img: { type: DataTypes.STRING, allowNull: false },
+});
+
+const ElectronicBook = sequelize.define("electronicBook", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true },
+  author: { type: DataTypes.STRING },
+  year: { type: DataTypes.INTEGER },
+  link: { type: DataTypes.STRING },
+  format: { type: DataTypes.STRING },
+  price: { type: DataTypes.INTEGER },
 });
 
 User.hasOne(Basket);
@@ -37,9 +47,9 @@ Book.hasMany(BasketBook);
 BasketBook.belongsTo(Book);
 
 module.exports = {
-    User,
-    Basket,
-    BasketBook,
-    Book
+  User,
+  Basket,
+  BasketBook,
+  Book,
+  ElectronicBook,
 };
-
