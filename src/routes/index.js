c0bfdb11ef72bac01a97fd9a6192878feb523c98
Albina3107 <71102725+Albina3107@users.noreply.github.com>
2021-12-28 02:36:32 +0200

    Add files via upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
-const Router = require('express');
+const Router = require("express");
 const router = new Router();
-const bookRouter = require('./bookRouter');
-const userRouter = require('./userRouter');
+const bookRouter = require("./bookRouter");
+const userRouter = require("./userRouter");
+const electronicBookRouter = require("./electronicBookRouter");
 
-router.use('/user', userRouter);
-router.use('/book', bookRouter);
-
+router.use("/user", userRouter);
+router.use("/book", bookRouter);
+router.use("/electro", electronicBookRouter);
 
 module.exports = router;
